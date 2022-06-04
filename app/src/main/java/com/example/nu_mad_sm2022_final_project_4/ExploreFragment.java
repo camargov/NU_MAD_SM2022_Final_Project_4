d62b64a8f8fff8c2fdd203fe084fb89cf5cddb66
Viviana Camargo <camargo5256@gmail.com>
2022-06-04 16:31:31 -0400

    Created frontend for explore page

--- app/src/main/java/com/example/nu_mad_sm2022_final_project_4/ExploreFragment.java
+++ app/src/main/java/com/example/nu_mad_sm2022_final_project_4/ExploreFragment.java
@@ -3,40 +3,25 @@ package com.example.nu_mad_sm2022_final_project_4;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ExploreFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ExploreFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
+    /*private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
     private String mParam1;
-    private String mParam2;
+    private String mParam2;*/
+    private SearchView searchView;
+    private TextView textViewSearchWord;
+    private RecyclerView recyclerView;
 
-    public ExploreFragment() {
-        // Required empty public constructor
-    }
+    public ExploreFragment() {}
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * //@param param1 Parameter 1.
-     * //@param param2 Parameter 2.
-     * @return A new instance of fragment ExploreFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static ExploreFragment newInstance() {
         ExploreFragment fragment = new ExploreFragment();
         Bundle args = new Bundle();
@@ -50,16 +35,19 @@ public class ExploreFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            //mParam1 = getArguments().getString(ARG_PARAM1);
+            //mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_explore, container, false);
+        searchView = view.findViewById(R.id.searchViewExplore);
+        textViewSearchWord = view.findViewById(R.id.textViewExploreSearchWord);
+        recyclerView = view.findViewById(R.id.recyclerViewExplore);
+
         getActivity().setTitle("Explore");
         return view;
     }
